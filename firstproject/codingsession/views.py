46d25580b8549a8a2ab4ee5a4bc5091b556ff708
bhaskarwagatakar <bhaskarwagatakar@gmail.com>
2021-04-06 11:58:00 +0530

    changing our template to display the coding sessions

--- firstproject/codingsession/views.py
+++ firstproject/codingsession/views.py
@@ -4,4 +4,6 @@ from codingsession.models import CodingSession
 # Create your views here.
 def codingsessionspage(request):
     coding_sessions = CodingSession.objects.all()
-    return render(request, template_name="codingsession/all.html")
+    return render(request, template_name="codingsession/all.html",
+                  context={'coding_sessions' : coding_sessions }
+                  )
